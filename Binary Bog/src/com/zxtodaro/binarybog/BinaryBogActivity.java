8a9bd10c5a5015af1f2f107e9eda9caa107eac9d
Zachary Todaro <zxtodaro@gmail.com>
2012-02-24 11:59:46 -0500

    Initial Commit of Senior Project

---
+++ Binary Bog/src/com/zxtodaro/binarybog/BinaryBogActivity.java
@@ -0,0 +1,13 @@
+package com.zxtodaro.binarybog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BinaryBogActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
